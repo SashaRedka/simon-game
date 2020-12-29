38e70a97983b7700347c2993993aceeb4995e84b
SashaRedka <dev.redka@gmail.com>
2020-12-29 23:32:20 +0200

    Added event delegation

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const genRandom = () => (Math.floor(Math.random() * 4));
+const generateRandom = () => Math.floor(Math.random() * 4);
 
-const buttons = document.querySelectorAll('.btn');
-const title = document.getElementById('level-title');
+const container = document.querySelector(".container");
+const buttons = document.querySelectorAll(".btn");
+const title = document.getElementById("level-title");
 
 let colorsState = [];
 let buttonsState = [];
@@ -10,80 +11,80 @@ let isGameOver = false;
 
 let currentBtn;
 let level = 0;
-let rand;
+let random;
 
-function clickButtonHandler() {
-  addAndRemoveClass(this, 'pressed')
-  buttonsState.push(this.getAttribute('id'));
 
-  buttonsState.forEach((btn, i) => {
-    if (btn !== colorsState[i]) {
-      isGameOver = true;
+function clickContainerHandler(e) {
+  if (e.target.tagName === "DIV" && e.target.classList.contains("btn")) {
+    const button = e.target;
+
+    toggleClass(button, "pressed");
+    buttonsState.push(button.getAttribute("id"));
+
+    buttonsState.forEach((btn, i) => {
+      if (btn !== colorsState[i]) {
+        isGameOver = true;
+      }
+    });
+
+    if (colorsState.length === buttonsState.length && !isGameOver) {
+      ++level;
+      generateNewButton();
     }
-  });
 
-  if (colorsState.length === buttonsState.length && !isGameOver) {
-    ++level;
-    genNewBtn();
-  }
-
-  if (isGameOver) {
-    gameOver();
+    if (isGameOver) {
+      gameOver();
+    }
   }
 }
 
 const newGame = () => {
   isGameOver = false;
-  window.addEventListener('keypress', keypressEvent);
+  window.addEventListener("keypress", keypressEvent);
 
   function keypressEvent() {
     startGame();
 
-    window.removeEventListener('keypress', keypressEvent);
+    window.removeEventListener("keypress", keypressEvent);
   }
 };
 newGame();
-
-const addAndRemoveClass = (el, className, delay = 100) => {
+const toggleClass = (el, className, delay = 100) => {
   el.classList.add(className);
-  setTimeout(() => {el.classList.remove(className);}, delay)
+  setTimeout(() => {
+    el.classList.remove(className);
+  }, delay);
 };
 
 const gameOver = () => {
-  title.innerHTML = 'Game over. Press a key to restart.';
-  addAndRemoveClass(document.body, 'game-over');
+  title.innerHTML = "Game over. Press a key to restart.";
+  toggleClass(document.body, "game-over");
   level = 0;
 
   colorsState = [];
   buttonsState = [];
 
-  buttons.forEach(btn => {
-    btn.removeEventListener('click', clickButtonHandler);
-  });
+  container && container.removeEventListener("click", clickContainerHandler);
 
   newGame();
 };
 
-const genNewBtn = () => {
+const generateNewButton = () => {
   title.innerHTML = `Level ${level + 1}`;
 
   buttonsState = [];
 
-  rand = genRandom();
-  currentBtn = buttons[rand];
+  random = generateRandom();
+  currentBtn = buttons[random];
 
-  setTimeout(() => {addAndRemoveClass(currentBtn, 'opacity', 510);}, 200)
+  setTimeout(() => {
+    toggleClass(currentBtn, "opacity", 510);
+  }, 200);
 
-  colorsState.push(currentBtn.getAttribute('id'));
-};
-
-const buttonsListeners = () => {
-  buttons.forEach(btn => {
-    btn.addEventListener('click', clickButtonHandler);
-  });
+  colorsState.push(currentBtn.getAttribute("id"));
 };
 
 const startGame = () => {
-  genNewBtn();
-  buttonsListeners();
+  generateNewButton();
+  container && container.addEventListener("click", clickContainerHandler);
 };
